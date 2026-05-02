e2609e78603fc815a45812fd09cdbc43a750e006
Jesus Mendoza <jesusmendozatecno23@gmail.com>
2026-05-02 13:32:59 -0400

    Migración a Laravel + estructura organizada

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,11 @@
 <?php
-// Web routes
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
